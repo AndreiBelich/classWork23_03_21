5a10d143488265954c8fd1e22b7ecf06afedd760
AndreiBelich <balichandrey89@gmail.com>
2021-03-23 20:52:40 +0300

    change index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,34 +3,34 @@ function isNumber(value){
   return Number(value) ? Number(value) : null;
 }
 
+function showResult(var1, var2, callback){
+  if(isNumber(var1) && isNumber(var2)){
+    console.log("Compare numbers")
+    console.log(callback(parseInt(var1), parseInt(var2)));
+  }
+  else if(var1 === null || var2 === null){
+    console.log("Один из аргументов равен null, сравнение невозможно!");
+  }
+  else{
+    console.log("Compare strings");
+    console.log(callback(var1, var2));
+  }
+}
+
 /* Task 1 function max*/
 function max(num1, num2){
-  if(isNumber(num1) && isNumber(num2)){
-    console.log("Сравнение чисел function max");
-    const tempNum1 = Number(num1);
-    const tempNum2 = Number(num2);
-    return tempNum1 > tempNum2 ? tempNum1 : tempNum2;
-  }
-  console.log("Сравнение разных величин function max:");
   return num1 > num2 ? num1 : num2;
 }
 
 /* task 2 function min */
 function min(num1, num2){
-  if(isNumber(num1) && isNumber(num2)){
-    console.log("Сравнение чисел function min");
-    const tempNum1 = Number(num1);
-    const tempNum2 = Number(num2);
-    return tempNum1 < tempNum2 ? tempNum1 : tempNum2;
-  }
-  console.log("Сравнение разных величин function min:");
   return num1 < num2 ? num1 : num2;
 }
 
 /* Task 3 */
 function isEven(value){
   if(!isNumber(value)){
-    console.log("it's not number");
+    console.log("it's not a number");
     return null;
   }
   return value % 2 === 0;
@@ -40,17 +40,18 @@ function isEven(value){
 function solve(a, b, c){
   if(!isNumber(a) || !isNumber(b) || !isNumber(c)){
     console.log("Wrong input");
-    return false;
+    return null;
   }
   if(a === 0){
     console.log("A === 0. It's mistake!!!");
-    return false;
+    return null;
   }
   const D = discriminant(a, b, c);
   console.log(`discriminant = ${D}`);
   let result = 0;
   if(D < 0){
-    result = "Корней нет";
+    console.log("Корней нет")
+    result = null;
   }
   else if(D === 0){
     result = (-b + Math.sqrt(D)) / (2 * a);
@@ -71,13 +72,13 @@ function discriminant(a, b, c){
 /*Task 1 tests*/
 const maxValue1 = prompt("Enter first max value:");
 const maxValue2 = prompt("Enter second max value:");
+showResult(maxValue1, maxValue2, max);
 
-console.log(max(maxValue1, maxValue2));
 
 /*Task 2 tests*/
 const minValue1 = prompt("Enter first min value:");
 const minValue2 = prompt("Enter second min value:");
-console.log(min(minValue1, minValue2));
+showResult(minValue1, minValue2, min);
 
 /*Task 3 tests*/
 console.log(isEven(12));
