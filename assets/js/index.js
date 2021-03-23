2e9d53a075f8e0c85df85637c39e8f2c266719eb
Andrey Belich <belichandrey89@gmail.com>
2021-03-23 11:07:05 +0200

    add function sum

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
-const hello = "Masha say: \\ \"HeLLO!\" \\";
+const foo = function(){
+  console.log(1);
+}
 
-console.log(hello);
+const sum = function(val1, val2){
+  const result = Number(val1) + Number(val2);
+  return result ? result : null;
+}
+
+console.log(sum(5,10));
+console.log(sum(1));
+console.log(sum('test', 5));
+console.log(sum("test", "test"));
+console.log(sum(5, "25"));
